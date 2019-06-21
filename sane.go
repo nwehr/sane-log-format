af37b7fc36b916f6f739bff9fcc42df03159cb24
Nathan Wehr <gtolemans@gmail.com>
2019-06-20 18:27:02 -0700

    Cleaned up a few things

--- sane.go
+++ sane.go
@@ -1,59 +1,50 @@
-/*
-Package sane impliments a function for converting
-php-style date/time format strings into go-style format strings
-*/
+// Package sane converts time formats from php-style to go-style
 package sane
 
-var mappedChars map[string]string
+func TimeFormat(saneFormat string) (goFormat string) {
+	goFormat = saneFormat
 
-// TimeFormat converts a sane, php-style format string to the equivilent go format
-// TimeFormat("Y-m-d H:i:s O") returns "2006-01-02 15:04:05 MST"
-func TimeFormat(saneFormat string) string {
-	format := saneFormat
-
-	for i := len(format) - 1; i >= 0; i-- {
-		insane := mappedChars[string(format[i])]
+	for i := len(goFormat) - 1; i > -1; i-- {
+		insane := mappedChars[string(goFormat[i])]
 
 		if len(insane) != 0 {
-			format = format[:i] + insane + format[i+1:]
+			goFormat = goFormat[:i] + insane + goFormat[i+1:]
 		}
 	}
 
-	return format
+	return goFormat
 }
 
-func init() {
-	mappedChars = map[string]string{
-		// year
-		"y": "06",
-		"Y": "2006",
+var mappedChars = map[string]string{
+	// year
+	"y": "06",
+	"Y": "2006",
 
-		// month
-		"n": "1",
-		"m": "01",
-		"M": "Jan",
-		"F": "January",
+	// month
+	"n": "1",
+	"m": "01",
+	"M": "Jan",
+	"F": "January",
 
-		// day
-		"j": "2",
-		"d": "02",
-		"D": "Mon",
-		"l": "Monday",
+	// day
+	"j": "2",
+	"d": "02",
+	"D": "Mon",
+	"l": "Monday",
 
-		// time
-		"a": "pm",
-		"A": "PM",
-		"g": "3",
-		"h": "03",
-		"H": "15",
-		"i": "04",
-		"s": "05",
-		"v": "000",
-		"u": "000000",
+	// time
+	"a": "pm",
+	"A": "PM",
+	"g": "3",
+	"h": "03",
+	"H": "15",
+	"i": "04",
+	"s": "05",
+	"v": "000",
+	"u": "000000",
 
-		// timezone
-		"T": "MST",
-		"O": "-0700",
-		"Q": "-07:00",
-	}
+	// timezone
+	"T": "MST",
+	"O": "-0700",
+	"Q": "-07:00",
 }
